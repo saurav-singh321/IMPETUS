95532a5348ac45dace9e14be4bee4645cafb2745
saurav-singh321 <56493291+saurav-singh321@users.noreply.github.com>
2022-08-19 22:06:21 +0530

    Delete Programs directory

--- Programs/2.Python Problems/1.Python begining/project01.py
+++
@@ -1,7 +0,0 @@
-input_rods = float(input("Input rods: "))
-input_rods = input_rods
-meters = round(5.0292*input_rods, 3)
-furlong = round(0.025*input_rods, 3)
-miles = round(0.00312*input_rods, 3)
-feet = round(16.50003*input_rods, 3)
-print(f"You input {input_rods} rods.\nConversions\nMeters: {meters}\nFeet: {feet}\nMiles: {miles}\nFurlongs: {furlong}")
